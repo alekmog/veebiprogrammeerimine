60b9159bc466b38aa1f087814a9ffdb9709ca95f
Aleksandr Mogilnõi <alekmog@users.noreply.github.com>
2018-09-10 10:41:03 +0300

    html muudetud php-ks

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>KoG</title>
+</head>
+<body>
+<hl> Welcome</hl>
+<p>September <a href="http://www.tlu.ee" target="_blank"> 10th</a>, 2018.</p>
+<!--<img src="../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_3.jpg" alt="TLÜ Terra õppehoone">-->
+<!--This means that this is a comment and means nothing else-->
+<img src="../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_3.jpg" alt="TLÜ Terra õppehoone">
+<p>Minu pinginaabri <a href="../~braijul/" target="_blank">veeb.</a>
+</body>
+</html>
